b9c8f7e07ac024922a61a28052ca9a1226106819
aranga-au <aranga.nanayakkara@gmail.com>
2016-11-20 23:12:00 +1100

    greeting

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('aws-lambda-http');
 //init routes
 var catRoute = require('./routes/category-route')(app);
 
-
+app.get('/',function(req,resp){
+   resp.send("Restful API based on AWS Lambda");
+});
 //aws lambda call entry
 exports.handler = app.handler;
