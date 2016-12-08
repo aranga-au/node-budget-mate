f3ddc9045bf6f61f5a2f33c3fd8a61add6a8590c
aranga-au <aranga.nanayakkara@gmail.com>
2016-12-08 21:28:06 +1100

    update aws-lambda-http to new version to make use of middleware feature

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var app = require('aws-lambda-http');
 //init routes
 var catRoute = require('./routes/category-route')(app);
 
+
+app.use(function (req,resp,next) {
+   console.log(req);
+   next();
+});
+
 app.get('/',function(req,resp){
    resp.send("Restful API based on AWS Lambda");
 });
