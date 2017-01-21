350f7788aaa96117abdabe793a8f7c3063b12193
aranga-au <aranga.nanayakkara@gmail.com>
2017-01-22 02:51:44 +1100

    minor bug fix

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -30,7 +30,7 @@ module.exports = function (aclManager, app) {
                 }
             });
             var payLoad = {
-                userId: userId,
+                userId: result.username,
                 loggedInAs: permissionDef.ADMIN,
             };
             
