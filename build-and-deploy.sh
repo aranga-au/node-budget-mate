b7b29194864a7275382e0ebd310e5ac4b7340cf7
aranga-au <aranga.nanayakkara@gmail.com>
2017-01-28 22:02:20 +1100

    update deployment

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,4 +1,5 @@
 git pull
 npm install
 grunt build
-aws s3 sync ./dist/. s3://repo.acnonline.net
+#aws s3 sync ./dist/. s3://repo.acnonline.net
+aws lambda update-function-code --function-name api-budget --zip-file fileb://./dist/node-budget-api.zip
